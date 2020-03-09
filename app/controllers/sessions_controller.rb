8e40a2819e500bbc8e59ff4fba8229d6a3ae4f42
Emmy White <emmalinerosewhite@gmail.com>
2020-03-09 14:41:34 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,3 +4,14 @@ class SessionsController < ApplicationController
       u.name = auth['info']['name']
       u.email = auth['info']['email']
       u.image = auth['info']['image']
+    end
+    session[:user_id] = @user.id
+    render 'welcome/home'
+  end
+  
+  private
+  
+  def auth
+    request.env['omniauth.auth']
+  end
+end
